408b16dc4866c488d9c00d380d353332c220f2fb
midhunhk <midhunhk@gmail.com>
2020-08-13 23:27:15 -0400

    complete search and details

--- src/components/BookStore.jsx
+++ src/components/BookStore.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import BooksDataService from '../api/BooksDataService'
 
 class BookStore extends Component {
@@ -8,14 +7,39 @@ class BookStore extends Component {
         super(props)
 
         this.state = {
-            searchTerm: 'The'
+            searchTerm: '',
+            screen: "empty",
+            selectedBook: null
         }
     }
 
     updateSearchTerm = (value) => {
-        console.log(value)
+        // Do Search in the parent component
+        if(value !== ""){
+            // Rest API Call here
+            BooksDataService.searchWithTitle(value)
+            .then( res => {
+                this.setState({
+                    searchTerm: value,
+                    books: res.data,
+                    selectedBook: null,
+                    screen: "results"
+                })
+            })
+            .catch( err => {
+                console.log(err)
+                this.setState({
+                    searchTerm: value,
+                    screen: "error"
+                })
+            } )
+        }
+    }
+
+    showBookDetails = (bookData) => {
         this.setState({
-            searchTerm: value
+            selectedBook: bookData,
+            screen: "details"
         })
     }
 
@@ -24,15 +48,14 @@ class BookStore extends Component {
             <div className="container BookStore">
                 <HeaderComponent />
                 <SearchComponent updateSearchTerm={this.updateSearchTerm} />
-                <Router>
-                    <>
-                        <Switch>
-                            <Route path="/" component={ResultsComponent} searchTerm={this.state.searchTerm} />
-                            <Route path="/details" component={BookDetailsComponent} />
-                            <Route component={ErrorComponent} />
-                        </Switch>
-                    </>
-                </Router>
+                
+                {this.state.screen === "results" 
+                        && <ResultsComponent result={this.state.books} 
+                                searchTerm={this.state.searchTerm}
+                                showBookDetails={this.showBookDetails}/>}
+                {this.state.screen === "details" && <BookDetailsComponent selectedBook={this.state.selectedBook} />}
+                {this.state.screen === "error" && <ErrorComponent searchTerm={this.state.searchTerm} />}
+                
                 <FooterComponent />
             </div>
         )
@@ -41,7 +64,6 @@ class BookStore extends Component {
 
 
 class SearchComponent extends Component {
-
     constructor(){
         super()
 
@@ -74,8 +96,8 @@ class SearchComponent extends Component {
                                         onChange={this.handleSearchTerm.bind(this)}/>
                                 </div>
                                 <div className="control">
-                                    <a className="button is-medium is-primary"
-                                        onClick={this.onSearch.bind(this)}>Search</a>
+                                    <button className="button is-medium is-primary"
+                                        onClick={this.onSearch.bind(this)}>Search</button>
                                 </div>
                             </div>
                         </div>
@@ -89,33 +111,33 @@ class SearchComponent extends Component {
 class ResultsComponent extends Component {
     constructor(props){
         super(props)
-        console.log(`ResultsComponent ${this.props.searchTerm}`)
+        console.log(`ResultsComponent ctor ${this.props.searchTerm}`)
         this.state = {
-            searchTerm: "The",
-            books: []
+            searchTerm: this.props.searchTerm,
+            books: this.props.result
         }
     }
 
-    componentDidMount(){
-        BooksDataService.searchWithTitle(this.state.searchTerm)
-        .then( res => {
-            this.setState({
-                books: res.data
-            })
-        })
-        .catch( err => console.log(err) )
+    static getDerivedStateFromProps(props, state){
+        console.log(`ResultsComponent ${props.searchTerm}`)
+        if(props.searchTerm !== state.searchTerm){
+            return {
+                searchTerm: props.searchTerm,
+                books: props.result
+            }
+        }
+        return null
     }
 
     render(){
         return (
             <section className="section pl-0 pr-0">
-                [{this.props.searchTerm}]
                 <div className="container has-text-left is-fluid pl-0 pr-0">
                     <div className="columns is-multiline">
                         {
                             this.state.books.map( book => 
                                 <div className="column is-half" key={book.isbn}>
-                                    <CardComponent book={book}/>
+                                    <CardComponent book={book} showBookDetails={this.props.showBookDetails}/>
                                 </div>
                             )
                         }
@@ -127,17 +149,13 @@ class ResultsComponent extends Component {
 }
 
 class CardComponent extends Component {
-    constructor(props){
-        super(props)
-    }
-
     render(){
         return (
             <div className="box">
                 <article className="media">
                     <div className="media-left">
                         <figure className="image is-64x64">
-                            <img src={this.props.book.url} alt="Image" />
+                            <img src={this.props.book.url} alt={this.props.book.title} />
                         </figure>
                     </div>
                     <div className="media-content">
@@ -148,8 +166,8 @@ class CardComponent extends Component {
                             {this.props.book.publisher} <br />
                             {this.props.book.publish_date}
                             </p>
-                            <a className="button mr-2">More Details</a>
-                            <a className="button is-primary">Buy</a>
+                            <button className="button mr-2" onClick={(event) => this.props.showBookDetails(this.props.book)}>More Details</button>
+                            <button className="button is-primary">Buy</button>
                         </div>
                     </div>
                 </article>
@@ -163,9 +181,29 @@ class CardComponent extends Component {
 class BookDetailsComponent extends Component {
     render(){
         return (
-            <div className="container">
-                BookDetailsComponent Component Here
-            </div>
+            <section className="section">
+                <div className="container">
+                <article className="media">
+                    <figure className="media-left">
+                        <p className="image is-64x64">
+                            <img src={this.props.selectedBook.url} alt={this.props.selectedBook.title} />
+                        </p>
+                    </figure>
+                    <div className="media-content">
+                        <div className="content has-text-left">
+                            <p>
+                                <strong>{this.props.selectedBook.title}</strong> <br />
+                                <small>By {this.props.selectedBook.author}</small><br />
+                                <small>ISBN: {this.props.selectedBook.isbn}</small><br />
+                                Publisher: {this.props.selectedBook.publisher} <br />
+                                Date of Publication: {this.props.selectedBook.publish_date} <br />
+                                Number of Pages: {this.props.selectedBook.numOfPages}
+                            </p>
+                        </div>
+                    </div>
+                    </article>
+                </div>
+            </section>
         )
     }
 }
@@ -179,15 +217,31 @@ function HeaderComponent() {
                         The BookStore
                     </p>
 
-                    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+                    <button className="navbar-burger" aria-label="menu" aria-expanded="false">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
-                    </a>
+                    </button>
                 </div>
             </nav>
         </header>
     )
+}
+
+class ErrorComponent extends Component {
+    render(){
+        return (
+            <section className="section">
+                <div className="container">
+                    <div className="content has-text-left has-text-danger">
+                        <p>
+                            No results for "{this.props.searchTerm}"
+                        </p>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
 
 function FooterComponent() {
@@ -199,15 +253,6 @@ function FooterComponent() {
                 </p>
             </div>
         </footer>
-    )
-}
-
-function ErrorComponent(){
-    return (
-        <div>
-            An Error has Occured. <br/>
-            Click here to navigate to <Link to={"/welcome"}>welcome page</Link>
-        </div>
     )
 }
 
